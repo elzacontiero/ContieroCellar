da50a2b0d9f42ad2553bf682f81164e0f22d9a7d
Elza Contiero <contieroelza@gmail.com>
2022-01-04 23:15:39 +0000

    CC-5 Create the initial Maven/Spring code skeleton in the repository

---
+++ src/main/java/com/contiero/cellar/ContieroCellarSpringApplication.java
@@ -0,0 +1,13 @@
+package com.contiero.cellar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContieroCellarSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContieroCellarSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contiero/cellar/ContieroCellarSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contiero.cellar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContieroCellarSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
