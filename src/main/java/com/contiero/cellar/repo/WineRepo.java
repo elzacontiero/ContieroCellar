f76dbc96b410005bd360c0d582e29ddcfd57d599
Elza Contiero <contieroelza@gmail.com>
2022-01-08 19:39:34 +0000

    CC-61 Create service unit test: retrieve wine entry by its ID

--- src/main/java/com/contiero/cellar/repo/WineRepo.java
+++ src/main/java/com/contiero/cellar/repo/WineRepo.java
@@ -1,11 +1,14 @@
 package com.contiero.cellar.repo;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.contiero.cellar.domain.Wine;
 
 public interface WineRepo extends JpaRepository<Wine, Long> {
-	
+
+	Optional<Wine> findById(long id);
+
 	List<Wine> findWineByProducer(String producer);
 
 	List<Wine> findWineByType(String type);
--- src/test/java/com/contiero/cellar/service/WineServiceUnitTest.java
+++ src/test/java/com/contiero/cellar/service/WineServiceUnitTest.java
@@ -2,12 +2,12 @@ package com.contiero.cellar.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
 import com.contiero.cellar.domain.Wine;
 import com.contiero.cellar.repo.WineRepo;
 
@@ -15,7 +15,7 @@ import com.contiero.cellar.repo.WineRepo;
 public class WineServiceUnitTest {
 
     @Autowired
-	private WineService service;
+    private WineService service;
     
     @MockBean
     private WineRepo repo;
@@ -23,7 +23,7 @@ public class WineServiceUnitTest {
     @Test
     public void createTest() {
         Wine in = new Wine("Barolo", "red", "Masi", 65.01, 1985, "Italy", 12);
-		Wine out = new Wine(123, "Barolo", "red", "Masi", 65.01, 1985, "Italy", 12);
+        Wine out = new Wine(123, "Barolo", "red", "Masi", 65.01, 1985, "Italy", 12);
 
         Mockito.when(repo.save(in)).thenReturn(out);
 
@@ -31,4 +31,24 @@ public class WineServiceUnitTest {
 
         Mockito.verify(repo, Mockito.times(1)).save(in);
     }
+
+    @Test 
+    public void retrieveByIDTest() {
+        Wine in = new Wine("Barolo", "red", "Masi", 65.01, 1985, "Italy", 12);
+        Wine out = new Wine(123, "Barolo", "red", "Masi", 65.01, 1985, "Italy", 12);
+        Mockito.when(repo.save(in)).thenReturn(out);
+
+        Optional<Wine> optOut = Optional.of(out);        
+        Mockito.when(repo.findById(123)).thenReturn(optOut);
+
+        Wine got = service.create(in);
+        assertEquals(in, got);
+        
+        got = service.getById(out.getId());
+        assertEquals(in, got);
+        Mockito.verify(repo, Mockito.times(1)).findById(out.getId());
+        
+    }
+
+
 }
